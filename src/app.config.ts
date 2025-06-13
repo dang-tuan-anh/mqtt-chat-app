a6b1956079a0a0e7be41a0a8df03efcc1a091e03
Dang Tuan Anh <tuananh1909@gmail.com>
2025-06-13 13:09:36 +0900

    add config

---
+++ src/app.config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  AWS_IOT_ENDPOINT: "xxxxxxxxxxxx-ats.iot.ap-northeast-1.amazonaws.com",
+  AWS_IOT_TOPIC_PUBLISH: "keybox/event/company01/5389a44cd046757e/status",
+  AWS_IOT_TOPIC_SUBSCRIBE: "keybox/action/company01/5389a44cd046757e/lock_command"
+};
